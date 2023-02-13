39933ff6345602446fb95dc4ff624bb8e6acc37e
LeviJames <2021155034@ub.edu.bz>
2023-02-13 13:15:34 -0800

    refractored code

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -5,9 +5,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
+	
 )
 
 func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,11 +13,16 @@ func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Reque
 }
 
 func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
+	/*params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"),10,64) 
 	if err != nil || id < 1 {
 		http.NotFound(w,r)
 		return
+	}*/
+	id, err := app.readIDParams(r)
+	if err != nil{
+		http.NotFound(w,r)
+		return 
 	}
 	fmt.Fprintf(w,"show details of school %d\n",id)
 }
